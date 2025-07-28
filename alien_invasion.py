6c2e8744355203ceebfac0178dd2e7a50d617b55
ndecker732 <nathandecker05@gmail.com>
2025-07-27 20:03:45 -0400

    p3 - fleshed out game stats to be updated by the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,7 +32,7 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         self.settings.initialize_dynamic_settings()
-        self.game_stats = GameStats(self.settings.starting_ship_count)
+        self.game_stats = GameStats(self)
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_w, self.settings.screen_h)
@@ -88,12 +88,13 @@ class AlienInvasion:
         if collisions:
             self.impact_sound.play()
             self.impact_sound.fadeout(500)
+            self.game_stats.update(collisions)
 
         # If the entire alien fleet is destoryed restart the level
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
             self.settings.increase_difficulty()
-            # update game stats
+            self.game_stats._update_level
             # update HUD view
 
     def _check_game_status(self):
@@ -113,7 +114,7 @@ class AlienInvasion:
 
     def restart_game(self):
         self.settings.initialize_dynamic_settings()
-        # reset Game stats
+        self.game_stats.reset_stats()
         # update HUD scores
         self._reset_level()
         self.ship._center_ship()
